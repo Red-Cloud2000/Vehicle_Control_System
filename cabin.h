1aaaaaea7f797ca28f3f6212770b0480a568147d
Abdelrahman Hesham <61296209+Red-Cloud2000@users.noreply.github.com>
2022-04-17 05:07:32 +0200

    Update cabin.h

--- cabin.h
+++ cabin.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "myFunctions.h"      //for including the functions declaration header file (Be_careful: it's a " " not < >, ;-> )
-#define switching 1           //for conditional compilation (for engine appearance)
+#define WITH_ENGINE_TEMP_CONTROLLER 1           //for conditional compilation (for engine appearance)
 							  //if you want engine work make it 1 if not make it 0
 
 enum devices_state{           //for ON/OFF states
@@ -19,7 +19,7 @@ struct{
 	int ACstate;              //AC state (ON/OFF)
 	int speed;                //Speed read
 
-#if switching                 //conditional compilation (for engine appearance)
+#if WITH_ENGINE_TEMP_CONTROLLER                 //conditional compilation (for engine appearance)
 
 	int eTempController;      //Engine temperature controller state (ON/OFF)
 	int engineTemp;           //Engine temperature read
